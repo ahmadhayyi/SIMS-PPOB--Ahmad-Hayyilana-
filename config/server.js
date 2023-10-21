100705bdaff1db7a10b5abf998703ed79b80ce32
ahmadhayyi <ahmadhayyi33@gmail.com>
2023-10-22 00:03:11 +0700

    start

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ const expressLayouts = require('express-ejs-layouts')
 
 /* CONFIG */
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 /* SET VIEW ENGINE */
 app.set('view engine', 'ejs')
